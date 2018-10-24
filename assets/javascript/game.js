8a06dc174b505b6ea88363cc82fc8aac5e759534
mccloud76 <dansolomon76@yahoo.com>
2018-10-24 12:03:36 -0700

    crystals have new random numbers after each win or loss

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -23,16 +23,9 @@ console.log(scoreCounter)
 var numberOptions = 0; 
 
 for (var i = 1; i < 5; i++) {
-
     var imageCrystal = $("#crystal" + [i]);
-
-    // imageCrystal.addClass("crystal-image");
-
-    // imageCrystal.attr("src", "http://cdn.playbuzz.com/cdn/35910209-2844-45c0-b099-f4d82878d54f/00261fda-4062-4096-81fd-8cf96b9034e8.jpg");
-
     numberOptions = [Math.floor(Math.random() * 11) + 1];
     imageCrystal.attr("data-crystalvalue", numberOptions);
-
     $("#crystals").append(imageCrystal);
 }
 
@@ -55,12 +48,25 @@ $(".crystal-image").on("click", function() {
         $("#target-number").text(targetNumber);        
         scoreCounter = 0;
         $("#score-counter").text(scoreCounter);
+        for (var i = 1; i < 5; i++) {
+            var imageCrystal = $("#crystal" + [i]);
+            numberOptions = [Math.floor(Math.random() * 11) + 1];
+            imageCrystal.attr("data-crystalvalue", numberOptions);
+            $("#crystals").append(imageCrystal);
+        }
     } else if (scoreCounter > targetNumber) {
         $("#loss-counter").text(lossCounter++);
         targetNumber = parseInt([Math.floor(Math.random() * 102) + 18]);
         $("#target-number").text(targetNumber);        
         scoreCounter = 0;
         $("#score-counter").text(scoreCounter);
+        for (var i = 1; i < 5; i++) {
+            var imageCrystal = $("#crystal" + [i]);
+            numberOptions = [Math.floor(Math.random() * 11) + 1];
+            imageCrystal.attr("data-crystalvalue", numberOptions);
+            $("#crystals").append(imageCrystal);
+        }
+        
     }
 
 });
