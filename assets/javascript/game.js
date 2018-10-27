400c5bb22240be2b7bf23982a2fa926ff5c00ac8
mccloud76 <dansolomon76@yahoo.com>
2018-10-27 09:54:53 -0700

    created a few functions to replace repeated lines of code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,18 @@
 // Javascript page for Unit-4-Game - Crystal Collector
 var scoreCounter = 0;
+var targetNumber = 0;
 
-var targetNumber = 0
 
-targetNumber = parseInt([Math.floor(Math.random() * 102) + 18]);
-$("#target-number").text(targetNumber);
+function generateRandomNumber(min, max) {
+    return parseInt(Math.floor(Math.random() * (max - min)) + min);
+}
 
+function setTargetNumber() {
+    targetNumber = generateRandomNumber(18, 120);
+    $("#target-number").text(targetNumber);
+}
+
+setTargetNumber();
 scoreCounter = 0;
 $("#score-counter").text(scoreCounter);
 console.log(scoreCounter, targetNumber)
@@ -20,27 +27,19 @@ $("#win-counter").text(winCounter);
 $("#loss-counter").text(lossCounter);
 
 console.log(scoreCounter)
-var numberOptions = 0; 
+var numberOptions = 0;
 
 var crystalValueArray = [];
 for (var i = 1; i < 5; i++) {
     var imageCrystal = $("#crystal" + [i]);
-    // function checkNumber(numberOptions){
-        numberOptions = [Math.floor(Math.random() * 11) + 1];
-        // if (crystalValueArray.indexOf(numberOptions) === -1) {
-                imageCrystal.attr("data-crystalvalue", numberOptions);
-                $("#crystals").append(imageCrystal);
-    //     } else {
-    //         checkNumber(numberOptions);
-    //     }
-    // }
+    imageCrystal.attr("data-crystalvalue", generateRandomNumber(1, 12));
+    $("#crystals").append(imageCrystal);
 
-   
 }
 
 
 
-$(".crystal-image").on("click", function() {
+$(".crystal-image").on("click", function () {
 
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
@@ -50,34 +49,30 @@ $(".crystal-image").on("click", function() {
 
     $("#score-counter").text(scoreCounter);
     console.log(scoreCounter, targetNumber);
-// debugger
+    // debugger
     if (scoreCounter == targetNumber) {
         winCounter++;
         $("#win-counter").text(winCounter);
-        targetNumber = parseInt([Math.floor(Math.random() * 102) + 18]);
-        $("#target-number").text(targetNumber);        
+        setTargetNumber();
         scoreCounter = 0;
         $("#score-counter").text(scoreCounter);
         for (var i = 1; i < 5; i++) {
             var imageCrystal = $("#crystal" + [i]);
-            numberOptions = [Math.floor(Math.random() * 11) + 1];
-            imageCrystal.attr("data-crystalvalue", numberOptions);
+            imageCrystal.attr("data-crystalvalue", generateRandomNumber(1, 12));
             $("#crystals").append(imageCrystal);
         }
     } else if (scoreCounter > targetNumber) {
         lossCounter++;
         $("#loss-counter").text(lossCounter);
-        targetNumber = parseInt([Math.floor(Math.random() * 102) + 18]);
-        $("#target-number").text(targetNumber);        
+        setTargetNumber();
         scoreCounter = 0;
         $("#score-counter").text(scoreCounter);
         for (var i = 1; i < 5; i++) {
             var imageCrystal = $("#crystal" + [i]);
-            numberOptions = [Math.floor(Math.random() * 11) + 1];
-            imageCrystal.attr("data-crystalvalue", numberOptions);
+            imageCrystal.attr("data-crystalvalue", generateRandomNumber(1, 12));
             $("#crystals").append(imageCrystal);
         }
-        
+
     }
 
 });
